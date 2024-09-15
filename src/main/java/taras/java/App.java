ff050a8d56a086c4622752e3c8368024b58a827d
Taras <zhiganovtaras@gmail.com>
2024-09-15 18:45:37 +0300

    Initial commit

---
+++ src/main/java/taras/java/App.java
@@ -0,0 +1,16 @@
+package taras.java;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //Gson
+    }
+}
